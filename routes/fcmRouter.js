f693a209a7e46c6f5f0736b123ce97032c9bd4e5
Taiwoshakirah <shakirattaiwo6@gmail.com>
2024-12-23 02:34:04 -0800

    student post fcm integration adding of authentication

--- routes/fcmRouter.js
+++ routes/fcmRouter.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const methodNotAllowed = require('../utils/methodNotAllowed')
 const updateFcmToken = require('../controllers/fcmTokenController')
+const authenticateToken = require('../middlewares/verifyToken')
 const router = express.Router()
 
 
-router.route('/update-fcm-token').put(updateFcmToken).all(methodNotAllowed)
+router.route('/update-fcm-token').put(authenticateToken,updateFcmToken).all(methodNotAllowed)
 
 
 module.exports = router

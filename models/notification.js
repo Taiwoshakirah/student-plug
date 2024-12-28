71ae88eaae2abae03d74f7439c69cdd5eda7aa4e
Taiwoshakirah <shakirattaiwo6@gmail.com>
2024-12-28 04:23:22 -0800

    websocket notification fetching with picture rect

--- models/notification.js
+++ models/notification.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 const notificationSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, required: true },
-  postId: { type: mongoose.Schema.Types.ObjectId, required: true },
   title: { type: String, required: true },
   body: { type: String, required: true },
   read: { type: Boolean, default: false }, // If the notification has been read

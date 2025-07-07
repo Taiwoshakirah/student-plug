d9c9e0d879f85bd61a14bd637feda1415c3b92fb
Taiwoshakirah <shakirattaiwo6@gmail.com>
2025-07-07 12:08:02 +0100

    changing back to senderaccount

--- utils/recordEventTransaction.js
+++ utils/recordEventTransaction.js
@@ -37,36 +37,30 @@ const mongoose = require("mongoose");
 //   return eventPayment;
 // };
 
-const recordEventTransaction = async (eventId, regNo, reference, amount) => {
-  // Find the EventPayment record
+const recordEventTransaction = async (eventId, senderAccountNumber, reference, amount) => {
   const eventPayment = await EventPayment.findOne({
     eventId: new mongoose.Types.ObjectId(eventId),
-    registrationNumber: regNo.trim(),
+    senderAccountNumber: senderAccountNumber.trim(),
   });
   if (!eventPayment) {
     throw new Error("EventPayment record not found for student.");
   }
 
-  // Update EventPayment fields
   eventPayment.paymentStatus = "completed";
   eventPayment.amountPaid = amount;
 
-  // Create EventTransaction
   const eventTransaction = new EventTransaction({
     transactionId: eventPayment._id,
     studentId: eventPayment.studentId,
     eventId: eventPayment.eventId,
     amountPaid: amount,
-    // paymentDate: new Date(),
     reference,
     paymentStatus: "completed",
   });
 
   await eventTransaction.save();
 
-  // Add the transaction to EventPayment's transactions array
   eventPayment.transactions.push(eventTransaction._id);
-  // Add reference to EventPayment for easy lookup
   eventPayment.reference = reference;
   await eventPayment.save();
 
